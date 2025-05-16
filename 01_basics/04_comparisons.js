a54c27ad3fefc5942018525c77dcd188871883e6
deepak-proponent <deepak.r@proponenttechnologies.com>
2025-05-16 17:55:03 +0530

    Comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,31 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+//  ===
+
+console.log("2" === 2); // false strict check
+console.log("2" == 2);  // true  loose check
+
+// Operator	Name	Example	Result
+// ==	Equal to (loose)	5 == '5'	true (type coercion)
+// ===	Equal to (strict)	5 === '5'	false (no coercion)
+// !=	Not equal to (loose)	5 != '5'	false
+// !==	Not equal to (strict)	5 !== '5'	true
+// >	Greater than	10 > 5	true
+// <	Less than	3 < 7	true
+// >=	Greater than or equal	4 >= 4	true
+// <=	Less than or equal	2 <= 1	false
