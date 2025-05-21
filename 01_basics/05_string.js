b5765ea8537471af71f504c6c38f90214ecb03f0
deepak-proponent <deepak.r@proponenttechnologies.com>
2025-05-21 17:18:36 +0530

    strings

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -3,11 +3,11 @@ const repoCount = 60
 
 // console.log(name + repoCount + " value");
 
-console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('riorawat-bi')
 
-console.log(typeof gameName);
+
+// console.log(typeof gameName);
 
 // keys value access
 
@@ -16,17 +16,39 @@ console.log(typeof gameName);
 
 // console.log(gameName.toUpperCase());
 
-console.log(gameName.charAt(3));
-console.log(gameName.indexOf('o'));
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('o'));
 
 // const newString = gameName.substring(0, 6)
 // console.log(newString);
+// const gameName = new String('riorawat-hi')
+// const anotherString = gameName.slice(-1, 11)
+// console.log(anotherString);
 
-const anotherString = gameName.slice(-2, 9)
-console.log(anotherString);
+// console.log(gameName.length);
 
-console.log(gameName.length);
+// **************************** Trim  **************************
 
+const newStringOne= "    riorawat    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+// ******************** Replace **********************
+
+const url = "https://riorawat.com/portfolio%20documents"
+
+url.replace('%20' , '-')
+
+console.log(url.replace('%20' , '-'));
+
+console.log(url.includes(('deepak')));
+
+const myArray = "rio-rawat-hyper"
+
+console.log(myArray.split('-'));
+
+console.log(name);
 
 
 
