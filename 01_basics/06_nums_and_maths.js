744fd33eb76147b1991183b1f1421c44dd85a178
deepak-proponent <deepak.r@proponenttechnologies.com>
2025-05-21 17:52:09 +0530

    numbers & math

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,49 @@
+// const score = 1234456
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// **********Exmaple********
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// ******* another exmaple *******
+
+const otherNumber = 121.343434
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ################ Maths #####################
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.8));
+// console.log(Math.min(1,2,3,4,5,8));
+// console.log(Math.max(1,2,3,4,5,8));
+
+console.log(Math.random());
+console.log(Math.random()*7 + 1);
+console.log(Math.floor(Math.random()*7) + 1);
+
+const min = 10
+const max = 20
+
+Math.random() * (max - min + 1) 
+
+console.log(Math.floor( Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
+
+
